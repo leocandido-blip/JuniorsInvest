659b29aead6e353e8108dadf4953341382bc154e
Leo <leocandido@MacBook-Air-von-Leo.local>
2025-11-12 22:25:35 -0500

    Final vercel routing fix

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,                // 👈 Dein lokaler Entwicklungsport
-    historyApiFallback: true,  // 👈 Damit React Router alle Pfade kennt
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
   build: {
-    outDir: "dist",
+    outDir: 'dist'
   },
-  base: "/",
+  server: {
+    port: 5173
+  },
+  base: './', // <--- DAS ist wichtig!
 });
+
